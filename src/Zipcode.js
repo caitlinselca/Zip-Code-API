afcde6c2f304fccd1c8aeb048e5a2bc16f95f4a0
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-01-09 11:59:18 -0500

    Consolidated Components, working zipcode lookup

--- src/Zipcode.js
+++ src/Zipcode.js
@@ -1,43 +1,90 @@
 import React, { Component } from "react";
-import Cards from "./Cards";
+import axios from "axios";
+
+const Card = props => {
+  return (
+    <div className="card-container">
+      <h2>{props.name}</h2>
+      <ul>
+        <li>State: {props.state}</li>
+        <li>Location: {props.location}</li>
+        <li>Population (estimated): {props.population}</li>
+        <li>Total Wages: {props.wages}</li>
+      </ul>
+    </div>
+  );
+};
 class Zipcode extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      zipcode: ""
+      zipcode: "",
+      zipInfo: [],
+      isError: false
     };
   }
 
   handleChange = e => {
-    this.setState({ zipcode: e.target.value });
+    this.setState({
+      zipcode: e.target.value
+    });
   };
 
   handleSubmit = e => {
     console.log("Zip Code: " + this.state.zipcode);
-    e.preventDefault();
+    axios
+      .get(`https://ctp-zip-api.herokuapp.com/zip/${this.state.zipcode}`)
+      .then(zip => {
+        console.log(zip);
+        let newState = zip.data.map(item => {
+          return {
+            name: item.LocationText,
+            state: item.State,
+            location: `(${item.Lat}, ${item.Long})`,
+            population: item.EstimatedPopulation,
+            wages: item.TotalWages
+          };
+        });
+        this.setState({ zipInfo: newState, isError: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ isError: true });
+      });
   };
 
   render() {
+    const Cards = this.state.zipInfo.map(item => (
+      <Card
+        name={item.name}
+        state={item.state}
+        location={item.location}
+        population={item.population}
+        wages={item.wages}
+      ></Card>
+    ));
+
     return (
       <div>
-        <div className="Header">
-          <h1>Zip Code Search</h1>
+        <div className="header">
+          <h1> Zip Code Search </h1>
         </div>
-        <div className="Search">
-          <label>Zip Code:</label>
+        <div className="content-container">
+          <label> Zip Codes: </label>
           <input
             type="number"
             value={this.state.zipcode}
             name="zipcode"
-            className="zipcode"
+            className="input-field"
             onChange={this.handleChange}
           ></input>
-          <button className="zip-search" onClick={this.handleSubmit}>
+          <button className="search-button" onClick={this.handleSubmit}>
             Search
           </button>
         </div>
-        <div className="Cards">
-          <Cards zipcode={this.state.zipcode}></Cards>
+        <div className="cards-container">
+          {this.state.isError ? <p> No Results </p> : ""}
+          {Cards}
         </div>
       </div>
     );
