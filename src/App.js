4a73b2a852241faaaaa3174fd7e828c1383f8d9b
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-01-09 13:01:44 -0500

    Created City component with simple render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Zipcode from "./Zipcode";
+import City from "./City";
 
 function App() {
   return (
     <div className="App">
       <Zipcode></Zipcode>
+      <City></City>
     </div>
   );
 }
---
+++ src/City.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class City extends Component {
+  render() {
+    return (
+      <div>
+        <div className="header-city">
+          <h1> City Search </h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default City;
